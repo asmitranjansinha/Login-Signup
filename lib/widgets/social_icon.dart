169d8d5b5c7b8e33485bda6f354d5b9076171336
asmy8h <sinhaasmit99@gmail.com>
2022-11-22 23:46:55 +0530

    change social-icon-button UI

--- lib/widgets/social_icon.dart
+++ lib/widgets/social_icon.dart
@@ -12,6 +12,20 @@ class SocialButtons extends StatelessWidget {
   Widget build(Object context) {
     return GestureDetector(
         onTap: ontap,
-        child: Image(image: AssetImage(img), width: 30));
+        child: Container(
+            height: 40,
+            width: 70,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.purple.shade200,
+              border: Border.all(color: Colors.purple.shade100, width: 3),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: Image(
+                image: AssetImage(img),
+                fit: BoxFit.scaleDown,
+              ),
+            )));
   }
 }

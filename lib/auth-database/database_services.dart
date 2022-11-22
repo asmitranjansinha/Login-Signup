e215c0e3248c831ff830fa8c9233dad57bff9b13
asmy8h <sinhaasmit99@gmail.com>
2022-11-23 01:37:55 +0530

    fized facebook/google signin for ios - added url schemes support for both the platform

--- lib/auth-database/database_services.dart
+++ lib/auth-database/database_services.dart
@@ -1,20 +1,20 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseServices {
-  late final String? uid;
-  DatabaseServices({this.uid});
-  final CollectionReference userCollection =
-      FirebaseFirestore.instance.collection("Users");
+// class DatabaseServices {
+//   late final String? uid;
+//   DatabaseServices({this.uid});
+//   final CollectionReference userCollection =
+//       FirebaseFirestore.instance.collection("Users");
 
-  Future savingUserData(String fullName, String email) async {
-    return await userCollection
-        .doc(uid)
-        .set({"fullName": fullName, "email": email, "uid": uid});
-  }
+//   Future savingUserData(String fullName, String email) async {
+//     return await userCollection
+//         .doc(uid)
+//         .set({"fullName": fullName, "email": email, "uid": uid});
+//   }
 
-  Future gettingUserData(String email) async {
-    QuerySnapshot snapshot =
-        await userCollection.where("email", isEqualTo: email).get();
-    return snapshot;
-  }
-}
+//   Future gettingUserData(String email) async {
+//     QuerySnapshot snapshot =
+//         await userCollection.where("email", isEqualTo: email).get();
+//     return snapshot;
+//   }
+// }
